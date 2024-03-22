1c503e1a407b6e68d4cf4b7b4462f66f0b9c2da6
Chee-Ho Nim <c.nim@se22.qmul.ac.uk>
2024-03-22 22:44:40 +0000

    Make a receipt model... what am I doing...

---
+++ app/models/receipt.py
@@ -0,0 +1,14 @@
+from app.extensions import db
+
+class Receipt(db.Model):
+	__tablename__ = "receipt"
+	id = db.Column(db.Integer, primary_key=True, autoincrement="auto")
+	title = db.Column(db.String(144))
+	image_uri = db.Column(db.Text)
+
+	def __repr__(self):
+		return f"Receipt ID: {self.id}"
+	#
+#
+
+# End of File
