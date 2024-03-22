9fc56147023ed4a1d248e0e89352c58b15f9b1fd
Chee-Ho Nim <c.nim@se22.qmul.ac.uk>
2024-03-22 22:54:28 +0000

    Claim model associates one-to-many with Receipt model

--- app/models/claim.py
+++ app/models/claim.py
@@ -6,6 +6,9 @@ class Claim(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     temp = db.Column(db.String(255))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    receipt_ids = db.relationship("Receipt", backref="Claim", lazy=True)
 
     def __repr__(self):
         return f"Claim ID: {self.id}"
+    #
+#
--- app/models/receipt.py
+++ app/models/receipt.py
@@ -2,7 +2,12 @@ from app.extensions import db
 
 class Receipt(db.Model):
 	__tablename__ = "receipt"
-	id = db.Column(db.Integer, primary_key=True, autoincrement="auto")
+	id = db.Column(
+		db.Integer, 
+		db.ForeignKey("Claim.id"), 
+		primary_key=True, 
+		autoincrement="auto"
+	)
 	title = db.Column(db.String(144))
 	image_uri = db.Column(db.Text)
 
