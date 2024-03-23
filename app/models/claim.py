96368a8e15f4fc7cfb83b4489156f041b3310627
Armin Shahnami <shahnami.armin@gmail.com>
2024-03-23 13:38:33 +0000

    Fix Receipt model and Claim relationship

--- app/models/claim.py
+++ app/models/claim.py
@@ -1,4 +1,5 @@
 from app.extensions import db
+from app.models.receipt import Receipt
 
 
 class Claim(db.Model):
@@ -6,7 +7,7 @@ class Claim(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     temp = db.Column(db.String(255))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    receipts = db.relationship("Receipt", backref="Claim", lazy=True)
+    receipts = db.relationship("Receipt", backref="claim", lazy=True)
 
     def __repr__(self):
         return f"Claim ID: {self.id}"
--- app/models/receipt.py
+++ app/models/receipt.py
@@ -7,9 +7,9 @@ class Receipt(db.Model):
 		primary_key=True, 
 		autoincrement="auto"
 	)
-	claim_id = db.Column(db.Integer, db.ForeignKey("claim.id")), 
 	title = db.Column(db.String(144))
 	image_uri = db.Column(db.Text)
+	claim_id = db.Column(db.Integer, db.ForeignKey("claim.id"))
 
 	def __repr__(self):
 		return f"Receipt ID: {self.id}"
