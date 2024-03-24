43d190d11d6cb47deb178843e9c7471118803a16
Armin Shahnami <shahnami.armin@gmail.com>
2024-03-24 12:54:23 +0000

    Fix migration

--- migrations/versions/a638e5850c77_add_status_column_to_claim_model.py
+++ migrations/versions/a638e5850c77_add_status_column_to_claim_model.py
@@ -8,8 +8,6 @@ Create Date: 2024-03-23 17:11:43.782127
 from alembic import op
 import sqlalchemy as sa
 
-from app.models.claim import ClaimStatus
-
 # revision identifiers, used by Alembic.
 revision = 'a638e5850c77'
 down_revision = 'd20d7edeb2b0'
@@ -18,9 +16,8 @@ depends_on = None
 
 
 def upgrade():
-    def upgrade():
-        op.add_column('claim',
-        sa.Column('status', sa.Enum(ClaimStatus))
+    op.add_column('claim',
+        sa.Column('status', sa.Enum('PENDING', 'APPROVED', 'DENIED', name='ClaimStatus'))
     )
 
 def downgrade():
