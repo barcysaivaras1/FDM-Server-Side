bac5ad09d954a5c18120774f367743c96f6af7a8
Chee-Ho Nim <ec221003@qmul.ac.uk>
2024-03-26 13:47:04 +0000

    Add title and amount fields to Claim model

--- app/models/claim.py
+++ app/models/claim.py
@@ -13,10 +13,12 @@ class ClaimStatus(enum.Enum):
 class Claim(db.Model):
     __tablename__ = 'claim'
     id = db.Column(db.Integer, primary_key=True)
-    temp = db.Column(db.String(255))
+    title = db.Column(db.String(255))
     status = db.Column(postgresql.ENUM(ClaimStatus))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     receipts = db.relationship("Receipt", backref="claim", lazy=True)
+    amount = db.Column(db.Float, nullable=False)
+    
 
     def __repr__(self):
         return f"Claim ID: {self.id}"
