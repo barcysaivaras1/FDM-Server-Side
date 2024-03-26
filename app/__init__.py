fa2096cffc3bb32b4a3372f0cce9f2267a352529
Armin Shahnami <shahnami.armin@gmail.com>
2024-03-26 20:47:35 +0000

    Update CORS allowed origins

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from app.extensions import db, login_manager, migrate, mail
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
-    CORS(app, supports_credentials=True)
+    CORS(app, supports_credentials=True, resources={r"/*": {"origins": "http://localhost:5173"}})
 
     db.init_app(app)
     login_manager.init_app(app)
